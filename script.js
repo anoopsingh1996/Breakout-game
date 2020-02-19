b9fc54fbd6663f4f0a76e55a7b0bf5eece595937
Anoop Singh <anoopraghuvansi@gmail.com>
2020-02-19 13:45:02 +0530

    add paddle

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const rules = document.getElementById('rules');
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+let score = 0;
+
+const brickRowCount = 9;
+const brickColumnCount = 5;
+
 //Create ball props
 const ball = {
   x: canvas.width / 2,
@@ -14,8 +19,20 @@ const ball = {
   dx: 4,
   dy: -4
 };
-// Draw ball on canvas
 
+// Create paddle props
+const paddle = {
+  x: canvas.width / 2 - 40,
+  y: canvas.height - 20,
+  w: 80,
+  h: 10,
+  speed: 8,
+  dx: 0
+};
+
+//Create brick props
+const brickInfo = {};
+// Draw ball on canvas
 function drawBall() {
   ctx.beginPath();
   ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
@@ -24,7 +41,29 @@ function drawBall() {
   ctx.closePath();
 }
 
-drawBall();
+// Draw paddle on canvas
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h);
+  ctx.fillStyle = '#0095dd';
+  ctx.fill();
+  ctx.closePath();
+}
+
+// Draw everthing
+function draw() {
+  drawBall();
+  drawPaddle();
+  drawScore();
+}
+
+// Draw score on canvas
+function drawScore() {
+  ctx.font = '20px Arial';
+  ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
+}
+
+draw();
 
 // Rules and close event handlers
 rulesBtn.addEventListener('click', () => rules.classList.add('show'));
